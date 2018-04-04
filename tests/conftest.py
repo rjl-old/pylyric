38a51a3ec6e69a0964b44eecc4e584048cf632db
richardjlyon <richard@richardlyon.net>
2018-04-04 07:28:30 +0100

    Move Lyric constructor to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,4 +8,21 @@
     https://pytest.org/latest/plugins.html
 """
 from __future__ import print_function, absolute_import, division
-# import pytest
+import pytest
+from pylyric.lyric import Lyric
+from pylyric.oauth2 import LyricClientCredentials
+import pylyric.config as cfg
+
+@pytest.fixture
+def lyric():
+    lcc = LyricClientCredentials(
+            client_id=cfg.CLIENT_ID,
+            client_secret=cfg.CLIENT_SECRET,
+            api_key=cfg.API_KEY,
+            access_token=cfg.ACCESS_TOKEN,
+            refresh_token=cfg.REFRESH_TOKEN,
+            redirect_url=cfg.REDIRECT_URL
+    )
+    lyric = Lyric(client_credentials_manager=lcc)
+    # lyric.trace_out = True
+    return lyric
--- tests/test_lyric.py
+++ tests/test_lyric.py
@@ -1,43 +1,25 @@
-from pylyric.lyric import Lyric
-from pylyric.device import Device
-from pylyric.oauth2 import LyricClientCredentials
-import pylyric.config as cfg
-
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-lcc = LyricClientCredentials(
-        client_id=cfg.CLIENT_ID,
-        client_secret=cfg.CLIENT_SECRET,
-        api_key=cfg.API_KEY,
-        access_token=cfg.ACCESS_TOKEN,
-        refresh_token=cfg.REFRESH_TOKEN,
-        redirect_url=cfg.REDIRECT_URL
-)
-
-lyric = Lyric(client_credentials_manager=lcc)
-# lyric.trace_out = True
-locationID = lyric.locations()[0]['locationID']
-device = lyric.locations()[0]['devices'][0]
-deviceID = lyric.locations()[0]['devices'][0]['deviceID']
-
-def test_locations():
+def test_locations(lyric):
     assert isinstance(lyric.locations(), list)
     assert isinstance(lyric.locations()[0], dict)
     assert 'locationID' in lyric.locations()[0]
 
 
-def test_devices():
+def test_devices(lyric):
+    locationID = lyric.locations()[0]['locationID']
     assert isinstance(lyric.devices(locationID), list)
     assert isinstance(lyric.devices(locationID)[0], dict)
 
 
-def test_device():
+def test_device(lyric):
+    locationID = lyric.locations()[0]['locationID']
+    deviceID = lyric.locations()[0]['devices'][0]['deviceID']
     assert isinstance(lyric.device(locationID, deviceID), dict)
 
 
-def test_change_device():
+def test_change_device(lyric):
+    locationID = lyric.locations()[0]['locationID']
+    deviceID = lyric.locations()[0]['devices'][0]['deviceID']
+    device = lyric.locations()[0]['devices'][0]
 
     # capture current state
     old_state = device['changeableValues']
