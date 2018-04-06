b87d5953cf0f4584c96f18a0c4b935ddf893360e
Richard Lyon <richard@richardlyon.net>
2018-04-06 21:59:24 +0100

    Define and test class Schedule()

--- pylyric/schedule.py
+++ pylyric/schedule.py
@@ -1,16 +1,40 @@
+from datetime import datetime
 
 
 class Schedule:
-    """Represents a heating schedule."""
+    """Represents a heating schedule.
+
+       A schedule is an active and an inactive period and temperature.
+    """
 
     def __init__(self,
-                     active_period_start=None,
-                     active_period_end=None,
-                     active_period_minimum_temperature=None,
-                     inactive_period_minimum_temperature=None
-                     ):
+                 active_period_start=None,
+                 active_period_end=None,
+                 active_period_minimum_temperature=None,
+                 inactive_period_minimum_temperature=None
+                 ):
 
-    self.active_period_start = active_period_start
-    self.active_period_end = active_period_endactive_period_minimum_temperature
-    self.active_period_minimum_temperature = active_period_minimum_temperature
-    self.inactive_period_minimum_temperature = inactive_period_minimum_temperature
+        self.active_period_start = active_period_start
+        self.active_period_end = active_period_end
+        self.active_period_minimum_temperature = active_period_minimum_temperature
+        self.inactive_period_minimum_temperature = inactive_period_minimum_temperature
+
+    def is_active_period(self):
+        """
+        :return: True if now() is in the active period
+        """
+        now = datetime.now()
+        if now >= self.active_period_start and now <= self.active_period_end:
+            return True
+        else:
+            return False
+
+    @property
+    def minimum_temperature(self):
+        """
+        :return: float The minimum temperature corresponding to the current period
+        """
+        if self.is_active_period():
+            return self.active_period_minimum_temperature
+        else:
+            return self.inactive_period_minimum_temperature
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,28 +2,3 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, absolute_import, division
 import pytest
-from pylyric.oauth2 import ApiCredentials
-from pylyric.lyric import Lyric
-from pylyric.device import Device
-import server.config as cfg
-
-lcc = ApiCredentials(client_id=cfg.CLIENT_ID, client_secret=cfg.CLIENT_SECRET, access_token=cfg.ACCESS_TOKEN,
-                     refresh_token=cfg.REFRESH_TOKEN)
-lyric_client = Lyric(credentials=lcc)
-
-locationID = lyric_client.get_locations()[0].location_id
-deviceID = lyric_client.get_locations()[0]._devices[0]['deviceID']
-device_dict = lyric_client.get_thermostat(locationID, deviceID)
-
-thermostat = Device(lyric_api=lyric_client, json=device_dict, location_id=locationID)
-
-
-@pytest.fixture(scope="module")
-def lyric():
-    # lyric.trace_out = True
-    return lyric_client
-
-
-@pytest.fixture(scope="module")
-def device():
-    return thermostat
