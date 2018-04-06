33a64e5a7442a47ebad396226cd9da70fdce10c9
Richard Lyon <richard@richardlyon.net>
2018-04-06 23:38:48 +0100

    Refactor temperature code

--- pylyric/environment_sensor.py
+++ pylyric/environment_sensor.py
@@ -4,10 +4,15 @@ import requests
 class EnvironmentSensor:
     """Base class for an environmental sensor"""
 
+    def __init__(self):
+        self.endpoint = "https://api.particle.io/v1/devices"
+        self.device_id = "37002b001147343438323536"
+        self.access_token = "51f8f2e01548da71585635f275914a49d383a4ae"
+
     @property
     def internal_temperature(self):
-        url = "https://api.particle.io/v1/devices/37002b001147343438323536/temperature"
-        params = {"access_token": "51f8f2e01548da71585635f275914a49d383a4ae"}
+        url = "{}/{}/temperature".format(self.endpoint, self.device_id)
+        params = {"access_token": self.access_token}
 
         r = requests.get(url, params=params)
         temperature = float(r.json()['result'])
