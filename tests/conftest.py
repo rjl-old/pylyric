6472bbdab0a42e25690a1843f5355dd28515b8fb
arlyon <arlyon@me.com>
2018-04-08 00:20:05 +0100

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,16 @@
 from __future__ import print_function, absolute_import, division
 import pytest
 
+from pylyric.heating_system import T6
 from pylyric.oauth2 import ApiCredentials
 from pylyric.lyric import Lyric
-from server import config as cfg
+from server import config as config
 
 credentials = ApiCredentials(
-        client_id=cfg.CLIENT_ID,
-        client_secret=cfg.CLIENT_SECRET,
-        access_token=cfg.ACCESS_TOKEN,
-        refresh_token=cfg.REFRESH_TOKEN
+    client_id=config.CLIENT_ID,
+    client_secret=config.CLIENT_SECRET,
+    access_token=config.ACCESS_TOKEN,
+    refresh_token=config.REFRESH_TOKEN
 )
 
 lyric_client = Lyric(credentials=credentials)
@@ -29,3 +30,8 @@ def lyric():
 @pytest.fixture(scope="module")
 def device():
     return thermostat
+
+
+@pytest.fixture(scope="module")
+def heating_system():
+    return lyric_client.get_device("199754", "LCC-00D02DB6B4A8", T6)
