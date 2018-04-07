385013b29f7aa576c47762522d19f9a6468786b0
arlyon <arlyon@me.com>
2018-04-07 10:58:23 +0100

    correct boiler

--- tests/test_boiler.py
+++ tests/test_boiler.py
@@ -1,9 +1,26 @@
 from pylyric.boiler import Boiler
+from pylyric.lyric import Lyric
+from pylyric.oauth2 import ApiCredentials
+from server import config
 
-b = Boiler()
+LOCATION_ID = "199754"
+DEVICE_ID = "LCC-00D02DB6B4A8"
+
+credentials = ApiCredentials(
+    client_id=config.CLIENT_ID,
+    client_secret=config.CLIENT_SECRET,
+    access_token=config.ACCESS_TOKEN,
+    refresh_token=config.REFRESH_TOKEN
+)
+
+
+lyric_client = Lyric(credentials=credentials)
+boiler = lyric_client.get_device(LOCATION_ID, DEVICE_ID, Boiler)
+
 
 def test_initialise():
-    assert isinstance(b, Boiler)
+    assert isinstance(boiler, Boiler)
+
 
 def test_turn_on():
-    b.turn_on()
+    boiler.turn_on()
