d2985cd15a9919387711e4c3bf4f7a6565dfeb4a
Richard Lyon <richard@richardlyon.net>
2018-04-07 10:46:17 +0100

    tweaks

---
+++ pylyric/heating_system.py
@@ -0,0 +1,8 @@
+class HeatingSystem:
+    """This class represents the heating system"""
+
+    def turn_on(self):
+        pass
+
+    def turn_off(self):
+        pass
--- tests/test_boiler.py
+++
@@ -1,9 +0,0 @@
-from pylyric.boiler import Boiler
-
-b = Boiler()
-
-def test_initialise():
-    assert isinstance(b, Boiler)
-
-def test_turn_on():
-    b.turn_on()
