a697c75302579369a5b8442221dcb61cdd0150e6
Richard Lyon <richard@richardlyon.net>
2018-04-07 14:08:48 +0100

    Add delay, factor out getting the T6 to utils

--- pylyric/controller.py
+++ pylyric/controller.py
@@ -1,30 +1,22 @@
 from pylyric.schedule import Schedule
-from pylyric.lyric import Lyric
+
 from pylyric.environment_sensor import EnvironmentSensor, Particle
 from pylyric.heating_system import HeatingSystem, T6
-from pylyric.oauth2 import ApiCredentials
-from pylyric.house import House
-from server import config as cfg
 
-from datetime import time
+from pylyric.house import House
+from pylyric.utils import get_a_lyric_device
+import datetime
+import time
 
 
 schedule = Schedule(
-        active_period_start=time(8,0),
-        active_period_end=time(22,0),
+        active_period_start=datetime.time(8,0),
+        active_period_end=datetime.time(22,0),
         active_period_minimum_temperature=20.0,
         inactive_period_minimum_temperature=18.0
 )
 
-credentials = ApiCredentials(
-        client_id=cfg.CLIENT_ID,
-        client_secret=cfg.CLIENT_SECRET,
-        access_token=cfg.ACCESS_TOKEN,
-        refresh_token=cfg.REFRESH_TOKEN
-)
-lyric_client = Lyric(credentials=credentials)
-location_id = lyric_client.get_locations()[0].location_id
-honeywell = lyric_client.get_devices(location_id)[0]
+honeywell = get_a_lyric_device()
 
 environment_sensor: EnvironmentSensor = Particle()
 heating_system: HeatingSystem = T6(honeywell)
@@ -33,8 +25,7 @@ house = House(environment_sensor=environment_sensor, heating_system=heating_syst
 # main Loop
 while True:
     current_temperature = house.environment_sensor.internal_temperature
-    required_temperature = schedule.minimum_temperature
-    is_too_cold = current_temperature < required_temperature
+    is_too_cold = current_temperature < schedule.minimum_temperature
 
     if schedule.is_active_period():
 
@@ -49,3 +40,6 @@ while True:
             house.heating_system.turn_on()
         else:
             house.heating_system.turn_off()
+
+    print(current_temperature)
+    time.sleep(10)
--- pylyric/utils.py
+++ pylyric/utils.py
@@ -2,6 +2,9 @@ import base64
 import json
 import os
 
+from server import config as cfg
+from pylyric.oauth2 import ApiCredentials
+from pylyric.lyric import Lyric
 
 def print_authorisation():
     """prints the base64 encoded result of id:secret
@@ -13,3 +16,16 @@ def print_authorisation():
 
     string = "{}:{}".format(config_data['client']['client_id'], config_data['client']['client_secret'])
     print(base64.b64encode(string.encode('utf-8')))
+
+
+
+def get_a_lyric_device():
+    credentials = ApiCredentials(
+            client_id=cfg.CLIENT_ID,
+            client_secret=cfg.CLIENT_SECRET,
+            access_token=cfg.ACCESS_TOKEN,
+            refresh_token=cfg.REFRESH_TOKEN
+    )
+    lyric_client = Lyric(credentials=credentials)
+    location_id = lyric_client.get_locations()[0].location_id
+    return lyric_client.get_devices(location_id)[0]
