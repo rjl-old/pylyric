a89ed25ba634f5c56d2eb7326bd472008cef5e4a
Richard Lyon <richard@richardlyon.net>
2018-04-10 12:47:43 +0100

    Delete redundant code

--- pylyric/utils.py
+++ pylyric/utils.py
@@ -18,18 +18,3 @@ def print_authorisation():
 
     string = "{}:{}".format(config_data['client']['client_id'], config_data['client']['client_secret'])
     print(base64.b64encode(string.encode('utf-8')))
-
-
-def get_the_t6():
-    credentials = ApiCredentials(
-        client_id=cfg.CLIENT_ID,
-        client_secret=cfg.CLIENT_SECRET,
-        access_token=cfg.ACCESS_TOKEN,
-        refresh_token=cfg.REFRESH_TOKEN
-    )
-    lyric_client = Lyric(credentials=credentials)
-    location_id = lyric_client.get_locations()[0].location_id
-    return lyric_client.get_devices(location_id)[0]
-
-def get_the_photon():
-    return Particle(auth_token=cfg.AUTH_TOKEN, device_id=cfg.DEVICE_ID)
