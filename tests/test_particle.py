b99e95298a7e773e8054d9649c45f7597653fbd1
Richard Lyon <richard@richardlyon.net>
2018-04-12 11:46:50 +0100

    Delete redundant code

--- tests/test_particle.py
+++
@@ -1,7 +0,0 @@
-from pylyric.particle import Particle
-import server.config as cfg
-
-
-def test_internal_temperature():
-    particle = Particle(auth_token=cfg.AUTH_TOKEN, device_id=cfg.DEVICE_ID)
-    assert isinstance(particle.internal_temperature, float)
